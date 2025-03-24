66e86ac9e5f3be2609fcc699fafefe93df3906dd
Stanislaw <stanislaw@janowicz.pl>
2025-03-24 10:45:42 +0100

    feature/SCRUM-45/Test ednpoint artykulu

---
+++ xUnitTests/ControllerTests/ArticleControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using MagazinEAPI.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MagazinEAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xUnitTests.ControllerTests
+{
+    public class ArticleControllerTests
+    {
+        [Fact]
+        public async Task nullRequest()
+        {
+            var controller = new AtricleController();
+            controller.ModelState.AddModelError("SessionName", "Required");
+
+
+            var result = await controller.Post("", "");
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task wrongEmailRequest()
+        {
+            var controller = new AtricleController();
+            controller.ModelState.AddModelError("SessionName", "Required");
+
+
+            var result = await controller.Post("Title", "em.adfadf.adfadfa");
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+        }
+    }
+}
