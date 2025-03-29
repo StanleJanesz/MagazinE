b91b25941ee8f96fcf849c35984eae4f5a3fd7cf
andrucior <106184210+andrucior@users.noreply.github.com>
2025-03-29 17:19:36 +0100

    feature: widok artykułu za paywallem

--- WebsiteFrontMagazinE/websitefrontmagazine.client/src/Pages/ArticlePage/ArticlePage.jsx
+++ WebsiteFrontMagazinE/websitefrontmagazine.client/src/Pages/ArticlePage/ArticlePage.jsx
@@ -4,24 +4,34 @@ import Box from '@mui/material/Box';
 import './ArticlePage.css';
 import { useNavigate } from 'react-router-dom';
 import Comment from '../../Components/Comment/Comment.jsx';
-function ArticlePage(articleId) {
+import { Button } from "bootstrap";
+function ArticlePage({ articleId }) {
     // TODO: PARSER BALD AND ITALICS
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [comments, setComments] = useState([]);
     const navigate = useNavigate();
+    const getRandomInt = (max) => Math.floor(Math.random() * max);
+    async function sleep(msec) {
+        return new Promise(resolve => setTimeout(resolve, msec));
+    }
 
     const dummyComments = [
         { author: "Jeanne Barbe", date: Date.now(), content: "Great news! I believe it will improve the results from students surveys!" },
         { author: "Paul deCat", date: Date.now(), content: "I'm excited about these new courses. Looking forward to AM4!" },
         { author: "Mr. Captain", date: Date.now(), content: "Will the new subjects have practical applications? Like Turing Machine!" },
     ];
-    useEffect(() => {
-        setLoading(true);
-        setData({
-            title: "Analiza Matematyczna 4 i Metody Numeryczne 3 juz od nowego roku na MiNI",
-            author: "Juan de Barbas",
-            content: `
+
+    const fetchComments = async () => {
+        setComments(dummyComments.map(comment => ({ ...comment })));
+    }
+
+    const fetchArticle = async (isUserPremium) => {
+        if (isUserPremium) {
+            setData({
+                title: "Analiza Matematyczna 4 i Metody Numeryczne 3 juz od nowego roku na MiNI",
+                author: "Juan de Barbas",
+                content: `
             
                 Wydzial Matematyki i Nauk Informacyjnych Politechniki Warszawskiej oglosil, ze od nowego roku akademickiego wprowadza dwa nowe przedmioty obowiazkowe: *Analiza Matematyczna 4* oraz *Metody Numeryczne 3*. Decyzja ta jest odpowiedzia na wieloletnie postulaty studentow o "wieksze wyzwania akademickie" oraz "prawdziwe odczucie studiow inzynierskich".
                 \n
@@ -35,9 +45,33 @@ function ArticlePage(articleId) {
 
                 Jak informuje jeden z wykladowcow: "Po trzecim semestrze analizy wielu studentow ma niedosyt. Czulismy, ze musimy im dac cos wiecej. Dlatego AM4 bedzie miala obowiazkowe projekty badawcze, a studenci na zaliczenie beda musieli napisac podrecznik do Analizy 5."`,
 
-        });
-        setComments(dummyComments);
+            });
+        }
+        else {
+            setData({
+                title: "Analiza Matematyczna 4 i Metody Numeryczne 3",
+                author: "Juan de Barbas",
+                content: "",
+            });
+        }
+    }
+
+    const fetchData = async () => {
+        setLoading(true);
+        await sleep(1000);
+        if (getRandomInt(10) % 2 === 0) {
+            // TODO: integrate secure checking with backend 
+            fetchArticle(true);
+        }
+        else {
+            fetchArticle(false);
+        }
+        fetchComments();
         setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchData();
     }, [articleId]);
 
 
@@ -55,7 +89,16 @@ function ArticlePage(articleId) {
                     </button>
                     <h1>{data.title}</h1>
                     <h2>Author: {data.author}</h2>
-                    <p>{data.content}</p>
+                    {data.content.length > 0 ? (
+                        <p>{data.content}</p>
+                    ) : (
+                        <div className="paywall">
+                            <p>This article is available for premium users only. Subscribe to unlock full access!</p>
+                            <button className="subscribeButton" onClick={() => navigate('/subscribe')}>
+                                Subscribe Now
+                            </button>
+                        </div>
+                    )}
                     <div className="commentsSection">
                         <h3>Comments:</h3>
                         {comments.length > 0 ? (
--- WebsiteFrontMagazinE/websitefrontmagazine.client/src/Pages/HomePage/HomePage.jsx
+++ WebsiteFrontMagazinE/websitefrontmagazine.client/src/Pages/HomePage/HomePage.jsx
@@ -1,35 +1,61 @@
 import Article from '../../Components/Article/Article';
 import { useState, useEffect } from 'react';
 import SearchBar from '../../Components/SearchBar/SearchBar';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function HomePage() {
-    const [content, setContent] = useState(null);
+    const [articles, setArticles] = useState([]); 
+    const [isLoading, setIsLoading] = useState(true);
 
-    const data = [
-        { title: "Analiza matematyczna 4 i Metody numeryczne 12 od nowego roku na MiNI" },
-        { title: "Another interesting topic" },
-        { title: "Another interesting topic2" },
-        { title: "Another interesting topic3" },
-        { title: "Another interesting topic4" },
-        { title: "Another interesting topic5" },
-        { title: "Another interesting topic6" },
-        { title: "Another interesting topic7" }
-    ];
+    async function sleep(msec) {
+        return new Promise(resolve => setTimeout(resolve, msec));
+    }
 
     useEffect(() => {
-        setContent(
-            data.map((article, index) => (
-                <Article articleData={article} key={index} />
-            ))
-        );
+        fetchData();
     }, []);
-    
-    return (
-        <div style={{ margin: '0, auto' }}>
-            <SearchBar />
-            {content}
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        await sleep(1100);
+
+        const data = [
+            { id: 0, title: "Analiza matematyczna 4 i Metody numeryczne 12 od nowego roku na MiNI", tags: "", isPremium: true },
+            { id: 1, title: "Another interesting topic", tags: "", isPremium: false },
+            { id: 2, title: "Another interesting topic2", tags: "", isPremium: false },
+            { id: 3, title: "Another interesting topic3", tags: "", isPremium: false },
+            { id: 4, title: "Another interesting topic4", tags: "", isPremium: false },
+            { id: 5, title: "Another interesting topic5", tags: "", isPremium: false },
+            { id: 6, title: "Another interesting topic6", tags: "", isPremium: false },
+            { id: 7, title: "Another interesting topic7", tags: "", isPremium: true }
+        ];
+
+        const articlesList = data.map(article => ({ ...article }));
+
+        setArticles(articlesList);
+        // console.log(articlesList);
+        setIsLoading(false);
+    };
+
+    const content = isLoading ? (
+        <CircularProgress />
+    ) : (
+            <div>
+                {articles.map(article => (
+                <Article data={article} />
+            ))}
         </div>
     );
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <SearchBar />
+            <div style={{ minHeight: '70vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                {content}
+            </div>
+        </div>
+    );
+
 }
 
 export default HomePage;
