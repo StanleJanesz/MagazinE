9bfc7e08cca12a6789555310d8651abf0ae45f40
wikiklimek <wikiklimek1@gmail.com>
2025-04-14 15:27:06 +0200

    report

--- MagazinEAPI/Models/Articles/Comments/CommentReport.cs
+++ MagazinEAPI/Models/Articles/Comments/CommentReport.cs
@@ -1,16 +1,32 @@
 ﻿using MagazinEAPI.Models.Users.Admins;
 using MagazinEAPI.Models.Users.Readers;
 using SharedLibrary.Base_Classes___Database;
+using SharedLibrary.DTO_Classes;
 namespace MagazinEAPI.Models.Articles.Comment
 {
     public class CommentReport : CommentReportAbstract
     {
         public Comment Comment { get; set; } = null!; // has 1 comment
-        public int ReportAuthorId { get; set; }
         public User ReportAuthor { get; set; } = null!; //has 1 report author
 
         public int? ManagedById { get; set; }
         public Admin? ManagedBy { get; set; } //has 0...1 ManagedBy (Admin)
+
+
+        public CommentReportDTO ToDTO()
+        {
+            return new CommentReportDTO()
+            { 
+                Id = this.Id,
+                CommentId = this.CommentId,
+                ReportAuthorId = this.ReportAuthorId,
+                Reason = this.Reason,
+                Date = this.Date,
+                State = this.State,
+                ManagedById = this.ManagedById ?? -1
+            };
+
+        }
     }
 
 }
