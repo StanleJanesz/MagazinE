7e5210ca98470790f528a799aefe64aa4e2e5b62
Wiktoria Klimek <132683108+wikiklimek@users.noreply.github.com>
2025-05-23 15:26:34 +0200

    Feature/scrum 12/widok banow i prosb admin (#45) (#48)
    * feat: widok list prosb - wstepnie
    
    * feat: widok prosb
    
    Co-authored-by: Andrzej Kupiec <106184210+andrucior@users.noreply.github.com>

--- WebsiteFrontMagazinE/websitefrontmagazine.client/src/Components/ListTile/ArticleTile/ArticleTile.jsx
+++ WebsiteFrontMagazinE/websitefrontmagazine.client/src/Components/ListTile/ArticleTile/ArticleTile.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import './ArticleTile.css';
 
-function ArticleTile({ title, photo, onSelect, id, sendToReview, rejectArticle, isChosen }) {
+function ArticleTile({ title, photo, onSelect, id, accept, reject, isChosen }) {
+
     const [photoPath, setPhotoPath] = useState(photo || 'src/assets/mini.jpg');
 
     const handleTileClick = (id) => {
@@ -9,7 +10,7 @@ function ArticleTile({ title, photo, onSelect, id, sendToReview, rejectArticle, 
     };
 
     return (
-        <div className={`at-container ${isChosen ? 'selected' : ''}`} onClick={() => handleTileClick(id)}>
+        <div className={isChosen ? 'at-selected' : 'at-container'} onClick={() => handleTileClick(id)}>
             <img
                 src={photoPath}
                 alt={title}
@@ -17,11 +18,10 @@ function ArticleTile({ title, photo, onSelect, id, sendToReview, rejectArticle, 
             />
             <h3 className="at-title">{title}</h3>
             <div className="at-buttonContainer">
-                <button className="actionButton" onClick={sendToReview}>
+                <button className="actionButton" onClick={accept}>
                     <img src="src/assets/accept.png" className="actionImage" />
                 </button>
-
-                <button className="actionButton" onClick={rejectArticle}>
+                <button className="actionButton" onClick={reject}>
                     <img src="src/assets/remove.png" className="actionImage" />
                 </button>
             </div>
