af36e0f2e3ebd77ceb11fd1bcd2ea4d21ecc0807
Stanisław Janowicz <147522953+StanleJanesz@users.noreply.github.com>
2025-06-09 09:54:45 +0200

    photos (#62)

--- MagazinEAPI/Program.cs
+++ MagazinEAPI/Program.cs
@@ -13,7 +13,9 @@ using Microsoft.OpenApi.Models;
 using Microsoft.Data.SqlClient;
 using System;
 using Microsoft.Extensions.Options;
+using Amazon.S3;
 using MagazinEAPI.Controllers;
+
 
 
 
@@ -59,6 +61,16 @@ builder.Services.AddControllers();
 //        };
 //    });
 
+builder.Services.AddSingleton<IAmazonS3>(sp =>
+{
+    var config = new AmazonS3Config
+    {
+        ServiceURL = "http://minio:9000",
+        ForcePathStyle = true // required for MinIO
+    };
+
+    return new AmazonS3Client("minioadmin", "minioadmin", config);
+});
 
 builder.Services.AddAuthentication(options =>
 {
